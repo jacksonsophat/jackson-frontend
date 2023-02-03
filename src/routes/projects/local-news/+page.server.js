cdf51e392f941d663fd291f57f7e7b6987ff8663
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2023-02-03 01:50:46 -0600

    Add Dallas to Local News

--- src/routes/projects/local-news/+page.server.js
+++ src/routes/projects/local-news/+page.server.js
@@ -9,8 +9,8 @@ export const load = async ({ fetch, cookies }) => {
 
 
     const fetchNews = async () => {
-        const res = await fetch(`http://127.0.0.1:8000/local-news/?select_city=${select_city}`)
-        // const res = await fetch(`http://150.136.7.63/local-news/?select_city=${select_city}`)
+        // const res = await fetch(`http://127.0.0.1:8000/local-news/?select_city=${select_city}`)
+        const res = await fetch(`http://150.136.7.63/local-news/?select_city=${select_city}`)
         // const res = await fetch('http://150.136.7.63/local-news/')
         const data = await res.json()
         return data
