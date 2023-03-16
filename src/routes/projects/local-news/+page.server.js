5f5cb9c1dbc2eba74237bb45eca3e8394f05eb8e
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2023-03-15 23:27:53 -0500

    update local new fetch api

--- src/routes/projects/local-news/+page.server.js
+++ src/routes/projects/local-news/+page.server.js
@@ -10,7 +10,7 @@ export const load = async ({ fetch, cookies }) => {
 
     const fetchNews = async () => {
         // const res = await fetch(`http://127.0.0.1:8000/projects/local-news/?select_city=${select_city}`)
-        const res = await fetch(`http://150.136.7.63/projects/local-news/?select_city=${select_city}`)
+        const res = await fetch(`http://150.136.7.63/local-news/?select_city=${select_city}`)
         const data = await res.json()
         return data
     }
