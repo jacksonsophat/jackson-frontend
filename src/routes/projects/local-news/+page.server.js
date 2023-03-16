bf91fe86c8e8b8f9d2e7a39c7283ce4a9bcf363e
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2023-03-15 23:14:38 -0500

    Fix local news

--- src/routes/projects/local-news/+page.server.js
+++ src/routes/projects/local-news/+page.server.js
@@ -9,8 +9,8 @@ export const load = async ({ fetch, cookies }) => {
 
 
     const fetchNews = async () => {
-        const res = await fetch(`http://127.0.0.1:8000/projects/local-news/?select_city=${select_city}`)
-        // const res = await fetch(`http://150.136.7.63/projects/local-news/?select_city=${select_city}`)
+        // const res = await fetch(`http://127.0.0.1:8000/projects/local-news/?select_city=${select_city}`)
+        const res = await fetch(`http://150.136.7.63/projects/local-news/?select_city=${select_city}`)
         const data = await res.json()
         return data
     }
